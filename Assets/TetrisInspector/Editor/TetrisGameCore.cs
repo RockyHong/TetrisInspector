3a70515a3fdff4725a35748f9f47b0a40b16eaad
ROCKYHONG <contact@rockyhong.com>
2018-12-23 02:47:25 +0800

    Input

---
+++ Assets/TetrisInspector/Editor/TetrisGameCore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisGameCore
+{
+    public void OnInput(TetrisInput input)
+    {
+        switch (input)
+        {
+            case TetrisInput.MoveLeft:
+                break;
+            case TetrisInput.MoveRight:
+                break;
+            case TetrisInput.Rotate:
+                break;
+            case TetrisInput.PushDown:
+                break;
+        }
+    }
+
+    public enum TetrisInput { PushDown, MoveLeft, MoveRight, Rotate }
+}
--- Assets/TetrisInspector/Editor/TetrisInspector.cs
+++ Assets/TetrisInspector/Editor/TetrisInspector.cs
@@ -3,19 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class TetrisInspector : EditorWindow {
+public class TetrisInspector : EditorWindow
+{
 
     [MenuItem("UselessTools/TetrisInspector")]
     static void OpenWindow()
     {
-        TetrisInspector tetrisInspector = GetWindow<TetrisInspector>();
+        var window = GetWindow<TetrisInspector>();
     }
 
-    
+    TetrisGameCore _tetrisGameCore;
+    TetrisGameCore tetrisGameCore
+    {
+        get
+        {
+            if (_tetrisGameCore == null)
+                _tetrisGameCore = new TetrisGameCore();
+            return _tetrisGameCore;
+        }
+    }
 
+    double lastGameViewTime;
+    private void Update()
+    {
+        Repaint();
+    }
+
+    float passiveUpdateTime;
     private void OnGUI()
     {
-        
+        KeyBoardInputDetect();
     }
 
+    void KeyBoardInputDetect()
+    {
+        Event e = Event.current;
+        if (e.type == EventType.KeyDown)
+        {
+            switch (Event.current.keyCode)
+            {
+                case KeyCode.UpArrow:
+                    tetrisGameCore.OnInput(TetrisGameCore.TetrisInput.Rotate);
+                    break;
+                case KeyCode.DownArrow:
+                    tetrisGameCore.OnInput(TetrisGameCore.TetrisInput.PushDown);
+                    break;
+                case KeyCode.RightArrow:
+                    tetrisGameCore.OnInput(TetrisGameCore.TetrisInput.MoveRight);
+                    break;
+                case KeyCode.LeftArrow:
+                    tetrisGameCore.OnInput(TetrisGameCore.TetrisInput.MoveLeft);
+                    break;
+            }
+        }
+    }
+
+    double lastFrameTime;
+    double _deltaTime;
+    double deltaTime
+    {
+        get
+        {
+            double currentTime = EditorApplication.timeSinceStartup;
+
+            if (currentTime == lastFrameTime)
+            {
+                return _deltaTime;
+            }
+            else
+            {
+                _deltaTime = currentTime - lastFrameTime;
+                lastFrameTime = currentTime;
+            }
+            return deltaTime;
+        }
+    }
 }
