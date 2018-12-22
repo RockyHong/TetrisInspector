1f757b2bda8ef10c5bd4dbb6b34b3811c996211e
ROCKYHONG <contact@rockyhong.com>
2018-12-22 21:39:05 +0800

    coordinate

---
+++ Assets/TetrisInspector/Editor/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Coordinate
+{
+    public int x;
+    public int y;
+
+    public Coordinate(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Coordinate operator +(Coordinate a, Coordinate b)
+    {
+        return new Coordinate(a.x + b.x, a.y + b.y);
+    }
+
+    public static Coordinate operator -(Coordinate a, Coordinate b)
+    {
+        return new Coordinate(a.x - b.x, a.y - b.y);
+    }
+}
---
+++ Assets/TetrisInspector/Editor/TetrisInspector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TetrisInspector : EditorWindow {
+
+    [MenuItem("UselessTools/TetrisInspector")]
+    static void OpenWindow()
+    {
+        TetrisInspector tetrisInspector = GetWindow<TetrisInspector>();
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
